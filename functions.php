c9a698d38756708f1c8a223beaad8fead00fadf8
TAKAYAMA Hiroshi <lamartakayama@gmail.com>
2022-09-05 12:23:57 +0900

    wordpressのプラグイン「admin bar position」のメニューをサポートする記述の追加

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 add_action('init', function() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+
+    /*メニューをサポート*/
+    register_nav_menus([
+        'global_nav' => 'グローバルナビゲーション',
+    ]);
 });
 
 /*アイキャッチ画像がなければ標準画像を取得する*/
